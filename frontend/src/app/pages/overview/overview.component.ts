35a8b384046809a99f1361e5120d58e0bfe98199
Bela <none_of_your_bees@wax.com>
2026-02-22 22:22:10 +0100

    first draft of overview screen food items

--- frontend/src/app/pages/overview/overview.component.ts
+++ frontend/src/app/pages/overview/overview.component.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
-// import { DatePipe } from '@angular/common';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DatePickerModule } from 'primeng/datepicker';
+import { MockDataService, FoodItem, FoodLogs } from '../../services/mock-data.service';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [FormsModule, DatePickerModule],
+  imports: [CommonModule, FormsModule, DatePickerModule],
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
   date: Date | undefined = new Date();
+  breakfastItems: FoodItem[] = [];
+  lunchItems: FoodItem[] = [];
+  dinnerItems: FoodItem[] = [];
+  isLoading = false;
+
+  constructor(
+    private mockDataService: MockDataService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    console.log(this.date);
+    this.loadFoodLogs();
+  }
+
+  loadFoodLogs() {
+    if (!this.date) return;
+
+    console.log('Loading food logs for date:', this.date);
+
+    this.isLoading = true;
+    this.mockDataService.getFoodLogs(this.date).subscribe({
+      next: (foodLogs: FoodLogs) => {
+        console.log('Food logs received:', foodLogs);
+        this.breakfastItems = foodLogs.breakfast;
+        this.lunchItems = foodLogs.lunch;
+        this.dinnerItems = foodLogs.dinner;
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error loading food logs:', error);
+        this.isLoading = false;
+      }
+    });
+  }
 
   increaseDate() {
     if (this.date) {
@@ -19,7 +56,7 @@ export class OverviewComponent {
       nextDate.setDate(nextDate.getDate() + 1);
       this.date = nextDate;
       console.log(this.date);
-      // update page content based on the new date
+      this.loadFoodLogs();
     }
   }
 
@@ -29,7 +66,22 @@ export class OverviewComponent {
       nextDate.setDate(nextDate.getDate() - 1);
       this.date = nextDate;
       console.log(this.date);
-      // update page content based on the new date
+      this.loadFoodLogs();
     }
+  }
+
+  deleteItem(mealType: 'breakfast' | 'lunch' | 'dinner', item: FoodItem) {
+    switch(mealType) {
+      case 'breakfast':
+        this.breakfastItems = this.breakfastItems.filter(i => i !== item);
+        break;
+      case 'lunch':
+        this.lunchItems = this.lunchItems.filter(i => i !== item);
+        break;
+      case 'dinner':
+        this.dinnerItems = this.dinnerItems.filter(i => i !== item);
+        break;
+    }
+    this.cdr.detectChanges();
   }
 }
