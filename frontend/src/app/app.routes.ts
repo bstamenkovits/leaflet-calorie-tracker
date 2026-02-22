a2f6fd5ceb736f6ed48559636faca7bc055b6f0f
Bela <none_of_your_bees@wax.com>
2026-02-22 16:27:03 +0100

    updated base set of pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { FoodLogComponent } from './pages/food-log/food-log.component';
-import { WeightLogComponent } from './pages/weight-log/weight-log.component';
+import { OverviewComponent } from './pages/overview/agent.component';
+import { LogComponent } from './pages/log/log.component';
 import { AgentComponent } from './pages/agent/agent.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/food-log', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'food-log', component: FoodLogComponent },
-  { path: 'weight-log', component: WeightLogComponent },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+  { path: 'overview', component: OverviewComponent },
   { path: 'agent', component: AgentComponent },
+  { path: 'log', component: LogComponent },
   { path: 'recipes', component: RecipesComponent },
   { path: 'profile', component: ProfileComponent }
 ];
